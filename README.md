1b80d076a44c80bfb097c3004b463033cef973d0
Gagandeep Singh <gagandeep.singh@joshtechnologygroup.com>
2017-02-05 16:20:14 +0530

    1. Finalized  nd tested script to automate sprint sheet -> jira work. 2. Added .ruby-version and .ruby-gemset 3. Updated README.md with ruby requirement

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ucm_scripts
 Cool scripts to automate work and save time ;)
+
+# Requirements
+ruby - 2.3.0
--- populate_jira_with_sprint_sheet_data.rb
+++ populate_jira_with_sprint_sheet_data.rb
@@ -1,58 +1,52 @@
-# SPs, assignees only
-
 require 'retrospectives'
 require 'json'
 include Retrospectives
 
+def success?(typhoeus_response)
+  unless typhoeus_response.code.to_s.start_with?('2')
+    puts "Response code #{typhoeus_response.code}"
+    puts "Response body #{typhoeus_response.body}"
+    false
+  else
+    true
+  end
+end
+
+STORY_POINT_CUSTOM_FIELD = 'customfield_10004'
 
 jira_options = {
   username: 'gagandeep.singh@kickdrumtech.com',
   password: 'gagan1dinesh',
-  site: 'https://copperegg.atlassian.net',
-  context_path: '',
-  auth_type: :basic,
-  http_debug: true
+  site: 'https://copperegg.atlassian.net'
 }
 
-members = { 'Gagan': 'gagandeep.singh',
-  'Neelakshi': 'Neelakshi',
-  'Sweta': 'SwetaSharma',
-  'Dinesh': 'DineshYadav',
-  'Ankit': 'ankit'
+members_username_mapping = { 'Gagan' => 'gagandeep.singh',
+  'Neelakshi' => 'Neelakshi',
+  'Sweta' => 'SwetaSharma',
+  'Dinesh' => 'DineshYadav',
+  'Ankit' => 'ankit'
 }
 
 retro = RetroSetup.new
 
 google_client = retro.authenticate_google_drive('../config.json')
-jira_client = retro.authenticate_jira(jira_options)
+jira_client = retro.authenticate_simple_jira(jira_options)
 
 sprint_sheet_key = '1UCBgSJkOJvMBZfAqAtlyQWakxkCqZ7kLO1nTCFX-GYA'
 ws = google_client.spreadsheet_by_key(sprint_sheet_key).worksheets[2]
 
-update_json = {
-  fields:
-  {
-    customfield_10004:4
-  }
-}
-
-(1..20).each do |row_index|
+(1..50).each do |row_index|
   next unless ws[row_index, 1].include?('CE-')
 
-  #puts "#{ws[row_index,1]}, #{ws[row_index,5]}"
-  update_json[:fields][:customfield_10004] = ws[row_index, 5]
-  parameters = {'fields'=> {'assignee'=> {'name'=> 'Neelakshi'}}}.to_json
-  headers = {"content-type" => "application/json"}
-  url = "https://copperegg.atlassian.net/rest/api/2/issue/#{ticket}"
-  auth = 'gagandeep.singh@kickdrumtech.com:gagan1dinesh'
+  ticket = ws[row_index, 1]
+  sp = ws[row_index, 5]
+  assignee = members_username_mapping[ws[row_index, 6]]
 
-  Typhoeus::Request.put(url, body: parameters, headers: headers, userpwd: auth, verbose: true)
+  next("incomplete data in row #{row_index}") if(ticket.nil? || ticket.empty? || sp.to_s.nil? ||
+                                                 sp.empty? || assignee.nil? || assignee.empty?)
 
-  break
+  resp1 = jira_client.update_assignee(assignee, ticket)
+  resp2 = jira_client.update_custom_field({ STORY_POINT_CUSTOM_FIELD => sp.to_i}, ticket)
+
+  puts "Success !  [#{ticket}]" if (success?(resp1) && success?(resp2))
 end
-
-
-
-
-#system("curl -D- -u gagandeep.singh@kickdrumtech.com:gagan1dinesh -X PUT -d $update -H \"Content-Type: application/json\" https://copperegg.atlassian.net/rest/api/2/issue/CE-2653")
-
